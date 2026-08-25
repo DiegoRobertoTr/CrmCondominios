07504858879d0728fb1d5c7d0f714139defc033a
DiegoRobertoTr <myjobtracecom@gmail.com>
2026-08-25 15:47:55 -0300

    Fix formatting in PLANOS list in pdf_generator.py

--- modules/pdf_generator.py
+++ modules/pdf_generator.py
@@ -36,6 +36,7 @@ MODELOS_ROTEADORES = [
 
 PLANOS = [
     "800MB+Canais: 59,99 Exclusivo Vibe Sunset",
+    "600MB+Tracecanais+Telecine+Premiere: 159,97",
     "600MB+Canais: 69,99",
     "600MB+Trace Canais Novo: 99,99",
     "800MB+Trace Canais Novo: 99,99",
